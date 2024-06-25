5f06d11b77266595556b1fbcb2f19400f515c967
romulolacerda <41117534+romulolacerda@users.noreply.github.com>
2024-06-24 22:08:40 -0300

    Update SonarcloudDemoApplication.java

--- src/main/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplication.java
+++ src/main/java/org/cornercoding/sonarclouddemo/SonarcloudDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SonarcloudDemoApplication {
 
 	public static void main(String[] args) {
+		String numero = 3;
 		SpringApplication.run(SonarcloudDemoApplication.class, args);
 	}
 }
